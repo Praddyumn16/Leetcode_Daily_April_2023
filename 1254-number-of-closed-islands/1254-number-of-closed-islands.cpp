4ee589381cdba6c8aadeeba1dd7864da2c55e748
Praddyumn Shukla <spraddyumn@gmail.com>
2023-04-08 22:54:25 +0530

    Time: 15 ms (51.22%), Space: 9.7 MB (43.37%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,37 +1,50 @@
 class Solution {
 public:
-    void dfs(int i, int j, vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] != 0)
+    int m , n;
+    
+    void bfs(int i , int j , vector<vector<bool>>& visited , vector<vector<int>>& grid) {
+        if(i == -1 || i == m || j == -1 || j == n || grid[i][j] || visited[i][j])
             return;
-
+        
+        visited[i][j] = 1;
         grid[i][j] = 1;
-        dfs(i+1, j, grid);
-        dfs(i-1, j, grid);
-        dfs(i, j+1, grid);
-        dfs(i, j-1, grid);
+        
+        bfs(i - 1 , j , visited , grid);
+        bfs(i + 1 , j , visited , grid);
+        bfs(i , j - 1 , visited , grid);
+        bfs(i , j + 1 , visited , grid);
     }
     
     int closedIsland(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-        for (int i = 0; i < m; i++) {
-            dfs(i, 0, grid);
-            dfs(i, n-1, grid);
-        }
-        for (int j = 0; j < n; j++) {
-            dfs(0, j, grid);
-            dfs(m-1, j, grid);
+        
+        m = grid.size() , n = grid[0].size();
+        vector<vector<bool>> visited(m , vector<bool>(n , 0));
+        
+        for(int i = 0 ; i < m ; i++) {
+            if(grid[i][0] == 0) // left
+                bfs(i , 0 , visited ,  grid);
+            if(grid[i][n - 1] == 0) // right
+                bfs(i , n - 1 , visited , grid);
         }
         
-        int count = 0;
-        for (int i = 1; i < m-1; i++) {
-            for (int j = 1; j < n-1; j++) {
-                if (grid[i][j] == 0) {
-                    dfs(i, j, grid);
-                    count++;
+        for(int i = 1 ; i < n - 1 ; i++) {
+            if(grid[0][i] == 0) // top
+                bfs(0 , i , visited , grid);
+            if(grid[m - 1][i] == 0) // bottom
+                bfs(m - 1 , i , visited , grid);
+        }
+        
+        int ans = 0;
+        
+        for(int i = 1 ; i < m - 1 ; i++) {
+            for(int j = 1 ; j < n - 1 ; j++) {
+                if(grid[i][j] == 0) {
+                    ans++;
+                    bfs(i , j , visited , grid);
                 }
             }
         }
-        return count;
+
+        return ans;
     }
 };
