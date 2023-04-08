adee6b41f89b98de5113709e51cf0160177c8fbd
Praddyumn Shukla <spraddyumn@gmail.com>
2023-04-08 22:26:28 +0530

    Time: 85 ms (36.27%), Space: 34.3 MB (16.66%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,35 +1,47 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& grid,int i,int j,int m,int n){
-        if(i<0 || i >=m || j<0 || j>=n || grid[i][j] == 0 || grid[i][j] == 2) return;
-        grid[i][j] = 2;
-        int ai[] = {0,1,0,-1};
-        int aj[] = {1,0,-1,0};
-
-        for(int k=0;k<4;k++){
-            dfs(grid,i+ai[k],j+aj[k],m,n);
-        }
+    int m , n;
+    
+    void bfs(int i , int j , vector<vector<bool>>& visited , vector<vector<int>>& grid) {
+        if(i == -1 || i == m || j == -1 || j == n || grid[i][j] == 0 || visited[i][j])
+            return;
+        
+        visited[i][j] = 1;
+        
+        grid[i][j] = 0;
+        
+        bfs(i - 1 , j , visited , grid);
+        bfs(i + 1 , j , visited , grid);
+        bfs(i , j - 1 , visited , grid);
+        bfs(i , j + 1 , visited , grid);
+        
     }
+    
     int numEnclaves(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-
-        for(int i=0;i<m;i++){
-            dfs(grid,i,0,m,n);
-            dfs(grid,i,n-1,m,n);
+        
+        m = grid.size() , n = grid[0].size();
+        vector<vector<bool>> visited(m , vector<bool>(n , 0));
+        
+        for(int i = 0 ; i < n ; i++) {
+            if(grid[0][i]) // top
+                bfs(0 , i , visited , grid);
+            if(grid[m - 1][i]) // bottom
+                bfs(m - 1 , i , visited , grid);
         }
-        for(int i=0;i<n;i++){
-            dfs(grid,0,i,m,n);
-            dfs(grid,m-1,i,m,n);
+        
+        for(int i = 1 ; i < m - 1 ; i++){
+            if(grid[i][0]) // left
+                bfs(i , 0 , visited , grid);
+            if(grid[i][n - 1]) // right
+                bfs(i , n - 1 , visited , grid);
         }
-
-        int count = 0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==1) count++;
-            }
-        }
-
-        return count;
+        
+        int ans = 0;
+        
+        for(auto a : grid)
+            for(auto b : a)
+                if(b) ans++;
+        
+        return ans;
     }
 };
