c2f811afe7b4c7fcf084dc6d72a4a8d7cd05ad7e
Praddyumn Shukla <spraddyumn@gmail.com>
2023-04-20 00:40:17 +0530

    Time: 322 ms (35.12%), Space: 60.9 MB (27.37%) - LeetHub

--- 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
+++ 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
@@ -1,26 +1,37 @@
 class Solution {
 public:
+    int mod = 1e9 + 7;
+    
+    long long solve(vector<vector<int>>& count , vector<string>& words, string& target , int col , int idx , vector<vector<int>>& dp) {
+        if(idx == target.size())
+            return 1;
+        if(col == words[0].size())
+            return 0;
+        
+        if(dp[col][idx] != -1)
+            return dp[col][idx];
+        
+        long long not_pick = solve(count , words , target , col + 1 , idx , dp);
+        long long pick = 0 , next = solve(count , words , target , col + 1 , idx + 1 , dp);
+        
+        pick = (next * count[col][target[idx] - 'a']) % mod;
+        
+        return dp[col][idx] = (pick + not_pick) % mod;
+    }
+    
+    
     int numWays(vector<string>& words, string target) {
-        int n = words[0].size();
-        int m = target.size();
-        int mod = 1000000007;
-        vector<int> dp(m+1, 0);
-        dp[0] = 1;
         
-        vector<vector<int>> count(n, vector<int>(26, 0));
-        for (const string& word : words) {
-            for (int i = 0; i < n; i++) {
+        int n = words.size() , m = words[0].size();
+        
+        vector<vector<int>> count(m , vector<int>(26 , 0));
+        vector<vector<int>> dp(m , vector<int>(target.size() , -1));
+        
+        for(auto word : words)
+            for(int i = 0 ; i < m ; i++)
                 count[i][word[i] - 'a']++;
-            }
-        }
         
-        for (int i = 0; i < n; i++) {
-            for (int j = m-1; j >= 0; j--) {
-                dp[j+1] += (int)((long)dp[j] * count[i][target[j] - 'a'] % mod);
-                dp[j+1] %= mod;
-            }
-        }
+        return solve(count , words , target , 0 , 0 , dp);
         
-        return dp[m];
     }
 };
