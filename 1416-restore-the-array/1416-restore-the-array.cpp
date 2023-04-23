10220da56199eb8df03f23958c8f68a87134b882
Praddyumn Shukla <spraddyumn@gmail.com>
2023-04-24 02:02:43 +0530

    Time: 2604 ms (5.30%), Space: 557.2 MB (5.29%) - LeetHub

--- 1416-restore-the-array/1416-restore-the-array.cpp
+++ 1416-restore-the-array/1416-restore-the-array.cpp
@@ -3,13 +3,14 @@ public:
     int mod = 1e9 + 7;
     
     int solve(int i , string curr , string& s , int k , vector<unordered_map<string , int>>& dp) {
+        
         if(i == s.size())
             return (stol(curr) <= k);
         
         if(dp[i].count(curr))
             return dp[i][curr];
         
-        int ans = 0 , add = 0 , not_add = 0;
+        int add = 0 , not_add = 0;
         
         if(stol(curr + s[i]) <= k)
             add = solve(i + 1 , curr + s[i] , s , k , dp);
