10211127f33ff985c5a4ae73687c3a2293403617
Praddyumn Shukla <spraddyumn@gmail.com>
2023-04-24 02:02:16 +0530

    Time: 2587 ms (5.30%), Space: 557.2 MB (5.29%) - LeetHub

---
+++ 1416-restore-the-array/1416-restore-the-array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int mod = 1e9 + 7;
+    
+    int solve(int i , string curr , string& s , int k , vector<unordered_map<string , int>>& dp) {
+        if(i == s.size())
+            return (stol(curr) <= k);
+        
+        if(dp[i].count(curr))
+            return dp[i][curr];
+        
+        int ans = 0 , add = 0 , not_add = 0;
+        
+        if(stol(curr + s[i]) <= k)
+            add = solve(i + 1 , curr + s[i] , s , k , dp);
+        
+        if(curr != "" && s[i] != '0')
+            not_add = solve(i + 1 , string(1 , s[i]) , s , k , dp);
+        
+        return dp[i][curr] = (add + not_add) % mod;
+    }
+    
+    int numberOfArrays(string s, int k) {
+        
+        vector<unordered_map<string , int>> dp(s.size());
+        return solve(1 , string(1 , s[0]) , s , k , dp);
+    }
+};
