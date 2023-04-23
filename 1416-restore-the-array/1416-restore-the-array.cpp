e655b6deae567d0851b29d7e2008e4cfb093cb6e
Praddyumn Shukla <spraddyumn@gmail.com>
2023-04-24 02:10:27 +0530

    Time: 260 ms (15.88%), Space: 43.1 MB (8.24%) - LeetHub

--- 1416-restore-the-array/1416-restore-the-array.cpp
+++ 1416-restore-the-array/1416-restore-the-array.cpp
@@ -2,28 +2,33 @@ class Solution {
 public:
     int mod = 1e9 + 7;
     
-    int solve(int i , string curr , string& s , int k , vector<unordered_map<string , int>>& dp) {
+    int solve(int i , string&s , int k , vector<int>& dp) {
         
         if(i == s.size())
-            return (stol(curr) <= k);
+            return 1;
         
-        if(dp[i].count(curr))
-            return dp[i][curr];
+        if(dp[i] != -1)
+            return dp[i];
         
-        int add = 0 , not_add = 0;
-        
-        if(stol(curr + s[i]) <= k)
-            add = solve(i + 1 , curr + s[i] , s , k , dp);
-        
-        if(curr != "" && s[i] != '0')
-            not_add = solve(i + 1 , string(1 , s[i]) , s , k , dp);
-        
-        return dp[i][curr] = (add + not_add) % mod;
+        if(s[i] == '0')
+            return 0;
+        else {
+            int ans = 0;
+            string curr = "";
+            for(int j = i ; j < s.size() ; j++) {
+                curr += s[j];
+                if(stol(curr) > k)
+                    break;
+                ans += solve(j + 1 , s , k , dp);
+                ans %= mod;
+            }
+            return dp[i] = ans;
+        }
     }
     
     int numberOfArrays(string s, int k) {
         
-        vector<unordered_map<string , int>> dp(s.size());
-        return solve(1 , string(1 , s[0]) , s , k , dp);
+        vector<int> dp(s.size() , -1);
+        return solve(0 , s , k , dp);
     }
 };
